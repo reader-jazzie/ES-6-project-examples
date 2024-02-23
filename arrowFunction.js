e68b12989bd577fce3e362048939e2a8cd51b76a
Jaspreet Kaur <reader.jaspreet@gmail.com>
2024-02-23 09:43:01 +0530

    My first commit

---
+++ arrowFunction.js
@@ -0,0 +1,11 @@
+/*const prompt=require('prompt-sync')();
+let a = parseInt(prompt("Enter the first number"));
+let b=parseInt(prompt("Enter the second number"));
+*/
+let sum=(a,b)=>{
+    sum=a+b;
+    return console.log(`the sum of ${a} and ${b} is: ${sum}`);
+
+}
+ 
+sum(7,6);
